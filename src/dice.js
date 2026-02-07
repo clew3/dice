d2cafa09b75a476688008dd3b84169b47f36ff8b
clew3 <38803188+clew3@users.noreply.github.com>
2026-02-07 17:31:29 +0800

    Update dice.js

--- src/dice.js
+++ src/dice.js
@@ -6,7 +6,7 @@
 
   Dice.prototype._roll = function() {
     min = Math.ceil(1);
-    max = Math.floor(7);
+    max = Math.floor(10);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
   };
 
